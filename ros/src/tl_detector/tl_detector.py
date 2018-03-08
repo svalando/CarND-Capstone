152c99227cab0aa4fd9757bc3e30dbad178b8c65
simonchu <simonchu47@gmail.com>
2018-03-09 00:13:30 +0800

    Implement tl detector function

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -10,8 +10,10 @@ from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
 import yaml
+import math
 
 STATE_COUNT_THRESHOLD = 3
+VISIBLE_DIST = 100
 
 class TLDetector(object):
     def __init__(self):
@@ -21,6 +23,7 @@ class TLDetector(object):
         self.waypoints = None
         self.camera_image = None
         self.lights = []
+        self.light_classifier = TLClassifier()
 
         sub1 = rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         sub2 = rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
@@ -36,12 +39,13 @@ class TLDetector(object):
         sub6 = rospy.Subscriber('/image_color', Image, self.image_cb)
 
         config_string = rospy.get_param("/traffic_light_config")
+        
         self.config = yaml.load(config_string)
-
+        rospy.loginfo('self.config is %s', self.config)
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        #self.light_classifier = TLClassifier()
         self.listener = tf.TransformListener()
 
         self.state = TrafficLight.UNKNOWN
@@ -55,7 +59,7 @@ class TLDetector(object):
         self.pose = msg
 
     def waypoints_cb(self, waypoints):
-        self.waypoints = waypoints
+        self.waypoints = waypoints.waypoints
 
     def traffic_cb(self, msg):
         self.lights = msg.lights
@@ -101,7 +105,32 @@ class TLDetector(object):
 
         """
         #TODO implement
-        return 0
+        dl = lambda a, b: math.sqrt((a.x-b.x)**2 + (a.y-b.y)**2  + (a.z-b.z)**2)
+        min_dist = dl(self.waypoints[0].pose.pose.position, pose.position)
+        closest_wp_id = 0
+        for i in range(1, len(self.waypoints)):
+            dist = dl(self.waypoints[i].pose.pose.position, pose.position)
+            if dist < min_dist:
+                min_dist = dist
+                closest_wp_id = i
+        
+        cur_x = pose.position.x
+        cur_y = pose.position.y
+        theta = math.atan2(cur_y, cur_x)
+        map_x = self.waypoints[closest_wp_id].pose.pose.position.x
+        map_y = self.waypoints[closest_wp_id].pose.pose.position.y
+
+        heading = math.atan2(map_y - cur_y, map_x - cur_x)
+        angle = math.fabs(theta - heading)
+        angle = min(2*math.pi - angle, angle)
+        if angle > math.pi/4:
+            next_wp_id = closest_wp_id
+        else:
+            next_wp_id = closest_wp_id - 1
+            if next_wp_id == -l:
+                next_wp_id = len(self.waypoints) - 1
+
+        return next_wp_id
 
     def get_light_state(self, light):
         """Determines the current color of the traffic light
@@ -131,23 +160,73 @@ class TLDetector(object):
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
 
         """
-        #light = None
-        light = True
-
+        light = None
+        tl_position = None
+        
         # List of positions that correspond to the line to stop in front of for a given intersection
         stop_line_positions = self.config['stop_line_positions']
         if(self.pose):
-            car_position = self.get_closest_waypoint(self.pose.pose)
+            #car_position = self.get_closest_waypoint(self.pose.pose)
+            light, tl_position, index = self.find_next_tl_stop(stop_line_positions, self.pose.pose)
 
         #TODO find the closest visible traffic light (if one exists)
         light_wp = []
         if light:
-            state = self.get_light_state(light)
-            rospy.loginfo('TL state is %d', state)
+            rospy.loginfo('tl_position is {}'.format(tl_position))
+            rospy.loginfo("Currently car at {}, {}".format(self.pose.pose.position.x, self.pose.pose.position.y))
 
-            return light_wp, state
-        self.waypoints = None
-        return -1, TrafficLight.UNKNOWN
+            # Using the classifier
+            #state = self.get_light_state(light)
+            # Using the simulator information
+            state = self.lights[index].state
+
+            rospy.loginfo('TL state is %d', state)
+            if state == TrafficLight.RED:
+                tl_pose = Pose()
+                tl_pose.position.x = tl_position[0]
+                tl_pose.position.y = tl_position[1]
+                light_wp_id = self.get_closest_waypoint(tl_pose)
+                light_wp.append(light_wp_id)
+                light_wp_x = self.waypoints[light_wp_id].pose.pose.position.x
+                light_wp_y = self.waypoints[light_wp_id].pose.pose.position.y
+                rospy.loginfo("light_wp[{}] at {}, {}".format(light_wp_id, light_wp_x, light_wp_y))
+                light_wp_x = self.waypoints[light_wp_id-1].pose.pose.position.x
+                light_wp_y = self.waypoints[light_wp_id-1].pose.pose.position.y
+                rospy.loginfo("former_wp[{}] at {}, {}".format(light_wp_id-1, light_wp_x, light_wp_y))
+                light_wp_x = self.waypoints[light_wp_id+1].pose.pose.position.x
+                light_wp_y = self.waypoints[light_wp_id+1].pose.pose.position.y
+                rospy.loginfo("next_wp[{}] at {}, {}".format(light_wp_id+1, light_wp_x, light_wp_y))
+                return light_wp_id, state
+            else:
+                return -1, state
+        else:
+            return -1, TrafficLight.UNKNOWN
+        #self.waypoints = None
+        #return -1, TrafficLight.UNKNOWN
+
+    def find_next_tl_stop(self, stop_line_positions, pose):
+        dl = lambda x1, y1, x2, y2: math.sqrt((x1-x2)**2 + (y1-y2)**2)
+        cur_x = pose.position.x
+        cur_y = pose.position.y
+        theta = math.atan2(cur_y, cur_x)
+        possible_tls = []
+        for i in range(len(stop_line_positions)):
+            tl_x = stop_line_positions[i][0]
+            tl_y = stop_line_positions[i][1]
+            dist = dl(tl_x, tl_y, cur_x, cur_y)
+            if dist < VISIBLE_DIST:
+                heading = math.atan2(tl_y - cur_y, tl_x - cur_x)
+                angle = math.fabs(theta - heading)
+                angle = min(2*math.pi - angle, angle)
+                if angle < math.pi/4:
+                     tl = (dist, stop_line_positions[i], i)
+                     possible_tls.append(tl)
+            
+            if len(possible_tls) > 0:
+                possible_tls = sorted(possible_tls)
+                return True, possible_tls[0][1], possible_tls[0][2]
+            else:
+                return False, None, None
 
 if __name__ == '__main__':
     try:
